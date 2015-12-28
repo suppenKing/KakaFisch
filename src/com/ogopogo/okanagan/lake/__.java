f7e044492e3cbbee966103917542c5ef2a0f74a0
suppenKing <p001678@10.239.128.155>
2015-12-28 10:40:06 +0100

    rr

--- src/com/ogopogo/okanagan/lake/__.java
+++ src/com/ogopogo/okanagan/lake/__.java
@@ -2,11 +2,11 @@ package com.ogopogo.okanagan.lake;
 
 public class __ {
   public int execute(int... args) {
-    o[] oo = { new o() {
+    ____[] ___ = { new ____() {
       public void _() {
         execute(21, 9, 15, 17, 9, 16, 20, 9, 28, 9, 26);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(21, 9, 15, 17, 9, 16, 20, 17, 9, 29, 9, 25, 20, 9, 28, 17, 9,
           15, 17, 9, 25, 16, 37, 9, 23, 9, 15, 17, 9, 28, 9, 25, 22, 9, 18, 30,
@@ -14,19 +14,19 @@ public class __ {
           15, 17, 9, 28, 9, 25, 22, 9, 18, 25, 18, 29, 9, 23, 15, 17, 9, 21, 9,
           16, 29, 17, 9, 26);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(14, 9, 31, 9, 23, 22, 17, 9, 22, 9, 16, 22, 9, 23);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         s.append(O[1]);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute();
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(9, 9, 18, 28, 9, 23, 22, 9, 32, 9, 23, 24, 31, 17, 9, 22, 9,
           23, 27, 17, 9, 28, 17, 9, 29, 9, 30, 16, 31, 9, 23, 28, 17, 9, 28,
@@ -34,176 +34,176 @@ public class __ {
           29, 9, 18, 20, 9, 11, 15, 9, 12, 9, 14, 17, 9, 11, 14, 17, 9, 22, 9,
           16, 19, 9, 23);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(14, 17, 9, 10, 17, 9, 18);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(5, 13);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(32, 17, 33, 15, 17, 9, 22, 9, 18, 20, 9, 15, 9, 23, 34);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         O[42 + 1 + O[42]++] = O[11];
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(12, 9, 15, 9, 16, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(20);
         O[11] = (O[11] == 0) ? execute(20, 33, 20) : execute(20, 20, 33);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(28, 9, 9, 16, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(10, 9, 12, 9, 6, 11);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(10, 17, 9, 9, 18, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(12, 9, 22, 9, 18, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(38, 39, 9);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         O[11] = O[O[11]];
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(38, 40, 9);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(10, 17, 9, 9, 18, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         O[11] = O[42 + 1 + --O[42]];
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         O[11] = execute();
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(21, 9, 9, 30, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(20);
         O[10] = O[11];
         execute(20);
         O[O[10]] = O[11];
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(10, 17, 9, 29, 9, 25, 15, 9, 30, 9, 15, 9, 23, 34);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(38, 41, 9);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(20);
         O[10] = O[11];
         execute(20);
         O[O[11]] = O[10];
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(12, 9, 22, 9, 16, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(22, 9, 9, 16, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(12, 9, 22, 9, 16, 28, 9, 25, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(38, 42, 9);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(12, 9, 28, 9, 16, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(15, 9, 15, 9, 25, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(O[11]);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(35, 9, 12, 9, 36, 9, 11);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(12, 9, 9, 16, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(15, 17);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(28, 9, 9, 16, 20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(20);
         O[8] = O[11];
         execute(20);
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         O[11] += O[8];
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         O[11] -= O[8];
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         O[11] *= O[8];
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         O[11] /= O[8];
       }
-    }, new o() {
+    }, new ____() {
       public void _() {
         execute(24, 14, 9, 9, 26, 13);
       }
     }, };
     for (int o : args)
-      oo[o]._();
+      ___[o]._();
     return 1860;
   }
 
   int[] O = null;
 
-  interface o {
+  interface ____ {
     void _();
   }
 
