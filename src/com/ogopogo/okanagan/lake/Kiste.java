b07ad7d215a2dafd73f9ad532928a00a48bcb8f2
suppenKing <p001678@10.239.128.155>
2016-03-03 13:51:38 +0100

    -

--- src/com/ogopogo/okanagan/lake/Kiste.java
+++ src/com/ogopogo/okanagan/lake/Kiste.java
@@ -1,6 +1,15 @@
 package com.ogopogo.okanagan.lake;
-
+/**
+ * 
+ * @param <T>
+ */
 public interface Kiste<T> {
  
+  /**
+   * I have to find a better job
+   * @param e  leere Kieste
+   */
   abstract void put(T e);
+  
+ 
 }
